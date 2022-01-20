f8ff295759a0920a5765e84ee2f4b6e600f6b277
Uticodes <etim.utibe@gmail.com>
2022-01-20 17:15:30 +0100

    ehhwey

---
+++ app/src/main/java/com/example/suga_social/CommentFragment.kt
@@ -0,0 +1,21 @@
+package com.example.suga_social
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CommentFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comment, container, false)
+    }
+
+}
